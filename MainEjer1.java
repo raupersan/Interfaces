df2d49554d5598eb83cfd68f2f7c6f334a2ee076
raupersan <157013125+raupersan@users.noreply.github.com>
2024-09-16 11:29:46 +0200

    Add files via upload

---
+++ MainEjer1.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+public class MainEjer1 {
+    public static void main(String[] args) {
+        // Crear instancias de Perro y Gato
+        Animal miPerro = new Perro();
+        Animal miGato = new Gato();
+        
+        miPerro.hacerSonido();  
+        miGato.hacerSonido();  
+    }
+}
---
+++ Perro.java
@@ -0,0 +1,6 @@
+
+public class Perro extends Animal{
+	public void hacerSonido(){
+	System.out.println("El perro dice: ¡Guau!");	
+	}
+}
