06ad7b05d5726c7163133d2c417c8f4b4e280a95
raupersan <raulps1798@gmail.com>
2024-10-04 14:19:17 +0200

    2

--- Desktop/AAD/ej_01_03/src/ej2/Main.java
+++ Desktop/AAD/ej_01_03/src/ej2/Main.java
@@ -6,16 +6,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
+import java.util.*;
 public class Main {
 
 	public static void escribirObjetos(String ruta) {
 
 		try {
+			ArrayList<Libros> biblioteca = new ArrayList<Libros>();
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(ruta)));
-			oos.writeObject(new Libros("a","b",1));
-			oos.writeObject(new Libros("c","d",2));
-			oos.writeObject(new Libros("c","d",3));
+			oos.writeObject(biblioteca.add(new Libros("a","b",1)));
+			oos.writeObject(biblioteca.add(new Libros("c","d",2)));
+			oos.writeObject(biblioteca.add(new Libros("c","d",3)));
 			oos.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -23,11 +24,13 @@ public class Main {
 	}
 
 	public static void lecturaObjetos(String  ruta) {
-
+		int i=0;
 		try {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(ruta)));
-			char a =(char) ois.read();
-			System.out.println(a);
+			
+			while ((i = ois.read()) != -1) {
+				System.out.print((char) i);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
